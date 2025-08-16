66fa777f54688e1a2d32fabdc9b23f7598ff22e8
rodrigocassiarimartinho <rodrigomartinho@gmail.com>
2025-08-15 21:04:54 -0300

    Create math.js

---
+++ js/math.js
@@ -0,0 +1,27 @@
+// js/math.js
+
+export function getNextPowerOfTwo(n) {
+    let p = 1;
+    while (p < n) p *= 2;
+    return p;
+}
+
+export function getWinnersBracketRoundsCount(bracketSize) {
+    if (bracketSize < 2) return 0;
+    return Math.log2(bracketSize);
+}
+
+export function getLosersBracketRoundsCount(bracketSize) {
+    if (bracketSize < 4) return 0;
+    return 2 * Math.log2(bracketSize) - 2;
+}
+
+export function getWinnersRoundMatchCount(vRoundIndex, bracketSize) {
+    return bracketSize / Math.pow(2, vRoundIndex);
+}
+
+export function getLosersRoundMatchCount(pRoundIndex, bracketSize) {
+    return bracketSize / Math.pow(2, Math.ceil(pRoundIndex / 2) + 1);
+}
+
+export const getLoserDestinationRound = (vRoundIndex) => (vRoundIndex === 1) ? 1 : (2 * vRoundIndex - 2);
