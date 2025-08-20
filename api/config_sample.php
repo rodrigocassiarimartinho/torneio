642339ac89754c425de1ff67d15fb4e5fee08318
rodrigocassiarimartinho <rodrigomartinho@gmail.com>
2025-08-19 21:14:01 -0300

    Create config_sample.php

---
+++ api/config_sample.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'coloque_o_nome_do_banco_aqui');
+define('DB_USER', 'coloque_o_usuario_aqui');
+define('DB_PASS', 'coloque_a_senha_aqui');
+// ... (resto do arquivo)
