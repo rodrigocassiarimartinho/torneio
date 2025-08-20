362e4a763771ba62dfbef560b48234d94eb2f2c8
rodrigocassiarimartinho <rodrigomartinho@gmail.com>
2025-08-19 22:04:00 -0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Bracket Generator</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js" type="module" defer></script>
+</head>
+<body>
+    <div id="setup">
+        <h1>Create New Tournament</h1>
+        
+        <div class="setup-section">
+            <input type="text" id="tournament-name" placeholder="Tournament Name" class="full-width-input">
+            <input type="date" id="tournament-date" class="full-width-input">
+            
+            <div class="tournament-type-selector">
+                <p>Select bracket type:</p>
+                <div>
+                    <input type="radio" id="type-single" name="bracket-type" value="single" checked>
+                    <label for="type-single">Single Elimination</label>
+                </div>
+                <div>
+                    <input type="radio" id="type-double" name="bracket-type" value="double">
+                    <label for-type-double">Double Elimination</label>
+                </div>
+            </div>
+            
+            <p class="instructions">Enter players one per line...</p>
+            <textarea id="player-list" placeholder="1. Isaac Newton..."></textarea>
+            
+            <button id="generate-btn">Generate and Save Tournament</button>
+        </div>
+    </div>
+
+    <div id="app-container" style="display: none;">
+        </div>
+</body>
+</html>
