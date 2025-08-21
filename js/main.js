86c40443c8c1063c08309b20c3d2137588fd6dbe
rodrigocassiarimartinho <rodrigomartinho@gmail.com>
2025-08-21 20:50:00 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -401,7 +401,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('back-to-list-btn').addEventListener('click', backToAction);
 
     } else {
-        document.getElementById('app-container')?.style.display = 'none';
+        document.getElementById('app-container').style.display = 'none';
         loadTournamentList();
     }
 });
